4502ff8b6360940e54b045ba752ee86e03dc87a1
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-20 15:20:25 +0700

    feat: add Input and InputLabel components

---
+++ resources/js/Components/Forms/Input.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { onMounted, Ref, ref } from "vue";
+import clsx from "clsx";
+import { Eye, EyeOff } from "lucide-vue-next";
+
+const input: Ref<HTMLInputElement | null> = ref(null);
+const isPasswordVisible = ref(false);
+
+const props = defineProps({
+    className: {
+        type: String,
+    },
+});
+
+const model = defineModel({
+    type: String,
+    required: true,
+});
+
+const togglePasswordVisibility = () => {
+    isPasswordVisible.value = !isPasswordVisible.value;
+};
+
+onMounted(() => {
+    if (input.value.hasAttribute("autofocus")) {
+        input.value.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+defineOptions({
+    inheritAttrs: false,
+});
+</script>
+
+<template>
+    <div
+        :class="
+            clsx(
+                'flex items-center rounded-md bg-zinc-800 border border-zinc-700 shadow-sm focus-within:border-emerald-500 focus-within:ring-2 focus-within:ring-emerald-500 has-[:disabled]:opacity-50 has-[:disabled]:cursor-not-allowed',
+                props.className
+            )
+        "
+    >
+        <input
+            class="rounded-md w-full px-3 py-1.5 bg-transparent border-none outline-none"
+            v-model="model"
+            ref="input"
+            v-bind="$attrs"
+            v-bind:type="
+                $attrs.type === 'password' && isPasswordVisible
+                    ? 'text'
+                    : 'password'
+            "
+        />
+        <div
+            v-if="$attrs.type === 'password'"
+            class="grid place-items-center pr-2"
+        >
+            <button
+                type="button"
+                @click="togglePasswordVisibility"
+                class="text-zinc-300 outline-none border-none focus:text-indigo-500"
+            >
+                <component :is="isPasswordVisible ? EyeOff : Eye" />
+            </button>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Components/Forms/InputLabel.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+    value: {
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <label class="text-base font-medium text-zinc-400">
+        <span v-if="value">{{ value }}</span>
+        <span v-else><slot /></span>
+    </label>
+</template>
